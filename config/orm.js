4b386545f5f92a83e2c030d97be657a969bbc968
tiffihaag <tiffihaag@gmail.com>
2016-09-29 17:26:10 -0400

    trying again

--- config/orm.js
+++ config/orm.js
@@ -5,3 +5,61 @@
 // updateOne()
 // Export the ORM object in module.exports.
 
+var connection = require('../config/connection.js');
+
+var orm = {
+	all: function (tableInput, cb) {
+		var queryString = 'SELECT * FROM ' + tableInput + ';';
+		connection.query(queryString, function (err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	},
+		// vals is an array of values that we want to save to cols
+		// cols are the columns we want to insert the values into
+	create: function (table, cols, vals, cb) {
+		var queryString = 'INSERT INTO ' + table;
+
+		queryString = queryString + ' (';
+		queryString = queryString + cols.toString();
+		queryString = queryString + ') ';
+		queryString = queryString + 'VALUES (';
+		queryString = queryString + printQuestionMarks(vals.length);
+		queryString = queryString + ') ';
+
+		console.log(queryString);
+
+		connection.query(queryString, vals, function (err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	},
+		// objColVals would be the columns and values that you want to update
+		// an example of objColVals would be {name: panther, sleepy: true}
+	update: function (table, objColVals, condition, cb) {
+		var queryString = 'UPDATE ' + table;
+
+		queryString = queryString + ' SET ';
+		queryString = queryString + objToSql(objColVals);
+		queryString = queryString + ' WHERE ';
+		queryString = queryString + condition;
+
+		console.log(queryString);
+		connection.query(queryString, function (err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	},
+	delete: function (table, condition, cb) {
+		var queryString = 'DELETE FROM ' + table;
+		queryString = queryString + ' WHERE ';
+		queryString = queryString + condition;
+
+		connection.query(queryString, function (err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	}
+};
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 //server.js
-
-var express 		= require('express');
-var bodyParser 		= require('body-parser');
-var connect     	= require('connect')
+var express 		 		= require('express');
+var bodyParser 			= require('body-parser');
 var methodOverride 	= require('method-override')
-var exphbs  		= require('express-handlebars');
-var app 			= express(); 
+var app 						= express(); 
 
 // override with the X-HTTP-Method-Override header in the request 
 app.use(methodOverride('X-HTTP-Method-Override'))
 
 //Express
 var app = express();
-//Serve static content for the app from the "public" directory 
-app.use(express.static(__dirname + '/public'));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 //handlebars
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
@@ -31,20 +34,26 @@ app.listen(3000);
 
 //--------
 //root get route
+//controllerjs
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+var port = 3000;
+app.listen(port);
+
+//get query
 app.get('/', function(req,res) {
 
   connection.query('SELECT * FROM burgers;', function(err, data) {
     if (err) throw err;
 
-    //console.log(data);
-
     //test it
-    //res.send(data);
+    console.log(data);
+    res.send(data);
 
     res.render('index', {burgers : data});
   });
 });
-
 
 //post route -> back to home
 app.post('/create', function(req, res) {
@@ -59,11 +68,19 @@ app.post('/create', function(req, res) {
   });
 });
 
-//--------
-
+//for if the user doesn't hit the right place
 app.use('/*', function(req,res){
    res.send("<h1>Not hungry?</h1>");
 });
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT);
+
+//orm
+//var orm = require('./config/orm.js');
+
+// orm.selectWhere('//////', '//////', '///////', function (err, res) {
+//   if (err) throw error;
+//   var data = res;
+//   console.log(data);
+// });
